75904530b0603b47725b0b3204cc921cc9b6595c
tomazws <tomazws@mac.com>
2024-03-27 23:31:23 -0700

    build

--- claude.py
+++ claude.py
@@ -29,7 +29,7 @@ def getCompletion(prompt):
             max_tokens = 1024,
             messages = st.session_state.messages
         )
-        st.session_state.messages.append({'role': 'assistant', 'content': response.content[0].ContentBlock})
+        st.session_state.messages.append({'role': 'assistant', 'content': response.content[0]})
         displayMessage('assistant', response.content[0].ContentBlock)
 
 ################################################################################
