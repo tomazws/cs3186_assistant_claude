94f76b5f8baace8d29e9ac10aa027d9b0eae7710
tomazws <tomazws@mac.com>
2024-03-27 22:49:40 -0700

    building

--- claude.py
+++ claude.py
@@ -1,23 +1,11 @@
 from anthropic import Anthropic
 import streamlit as streamlit
-import uuid
 
 ################################################################################
 ##                           INITIALIZE APPLICATION                           ##
 ################################################################################
 # Initialize OpenAI Assistant API
 client = Anthropic(api_key=st.secrets['CLAUDE_API_KEY'])
-
-# Initialize session state variables
-if 'session_id' not in st.session_state:
-    st.session_state.session_id = str(uuid.uuid4())
-
-if 'thread' not in st.session_state:
-    st.session_state.thread = client.beta.threads.create(
-        metadata={
-            'session_id': st.session_state.session_id,
-        }
-    )
 
 if 'messages' not in st.session_state:
     st.session_state.messages = []
