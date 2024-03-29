8bb96f50a0519d3ad5739ec77ab12982ef359cd8
tomazws <tomazws@mac.com>
2024-03-28 22:04:45 -0700

    updated output parser

--- claude.py
+++ claude.py
@@ -28,7 +28,7 @@ def displayMessage(role, content):
                 string_pos = 0
                 for match in re.finditer('```dot[^}]*}\n```|digraph.*{[^}]*}', item['text']):
                     st.write(item['text'][string_pos: match.start() - 1])
-                    st.graphviz_chart(match.group())
+                    st.graphviz_chart(match.group() if re.search('```dot', match.group()) == -1 else match.group()[6: -3])
                     string_pos = match.end() + 1
                 st.write(item['text'][string_pos:])
     st.write('')
